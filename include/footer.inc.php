99756bc517435150a2ed0a8848b0ba3ddca91cab
existenzin <89390232+Tenzinxo@users.noreply.github.com>
2025-04-20 00:27:31 +0200

    Add files via upload

---
+++ include/footer.inc.php
@@ -0,0 +1,9 @@
+<footer>
+	<ul>
+		<li><strong><a href="plan.php">Annexe et Plan du site</a></strong></li>		
+	</ul>
+	<p>Vous utilisez actuellement <strong><?php echo getNavigateur(); ?></strong>.</p>
+	<p>Group C-22 © 2025 | Tous droits réservés.</p>
+</footer>
+</body>
+</html>
